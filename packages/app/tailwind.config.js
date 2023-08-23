a286136236f9df111a228575dfb48896b73f1898
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2023-08-23 22:43:27 +0200

    shadow for header added

--- packages/app/tailwind.config.js
+++ packages/app/tailwind.config.js
@@ -1,23 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-    content: ["./src/**/*.{html,js,tsx}"],
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: [
-                    'Inconsolata',
-                    ...defaultTheme.fontFamily.sans
-                ],
-                mono: [
-                    'Inconsolata',
-                    ...defaultTheme.fontFamily.mono
-                ],
-            }
-        },
+  content: ["./src/**/*.{html,js,tsx}"],
+  theme: {
+    extend: {
+      dropShadow: {
+        "3xl": "0 3px 3px rgba(245, 158, 11, 0.25)",
+      },
+      fontFamily: {
+        sans: ["Inconsolata", ...defaultTheme.fontFamily.sans],
+        mono: ["Inconsolata", ...defaultTheme.fontFamily.mono],
+      },
     },
-    plugins: [],
-}
-
+  },
+  plugins: [],
+};
